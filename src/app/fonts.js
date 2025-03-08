957aea0b85773fb5ab1483d691ace83bda1e868b
Sahan Perera <sahanvimukthi00@gmail.com>
2025-03-08 22:25:56 +0530

    chore: removed all files

--- src/app/fonts.js
+++
@@ -1,6 +0,0 @@
-import { Poppins } from "next/font/google";
-
-export const poppins = Poppins({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
-});
